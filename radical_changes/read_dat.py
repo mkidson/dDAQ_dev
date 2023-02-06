f1caa4e6cfdf1a0bc92fe375b902f32111bd934d
mkidson <miles.kidson@gmail.com>
2023-02-06 14:36:09 +0200

    tweaks and making some comments in radical_changes

--- radical_changes/read_dat.py
+++ radical_changes/read_dat.py
@@ -138,7 +138,7 @@ class read_dat(object):
                 print('Please input an array if you want to apply cuts. Defaulting to no cuts')
             else:
                 cuts = np.array(cuts)
-                if inc == None:
+                if inc == None: # Setting inc to all 1, so all included
                     inc = np.ones(len(cuts))
                 elif len(inc) != len(cuts):
                     print('ERROR: len(inc) must be the same as len(cuts). Returning None')
@@ -147,7 +147,8 @@ class read_dat(object):
                     inc = np.array(inc)
 
 
-        #initiate the output files for the traces and other parameters, one per channel
+        # initiate the output files for the traces and other parameters, one per channel
+        # only doing the headers now, no data yet
         for i in range(len(ch)):
             if output != False:
                 header = [f'{self.fileName[:-4]} channel {ch[i]}, {events} events, cuts {cuts}']
@@ -157,7 +158,7 @@ class read_dat(object):
                     f = open(filename, 'w', newline='')
                 writer_params.append(csv.writer(f))
                 writer_params[i].writerow(header)
-                if len(self.tLong) > 1 or len(self.tShort) > 1:
+                if len(self.tLong) > 1 or len(self.tShort) > 1: # idk what this is doing
                     writer_params[i].writerow([f'long integral (ns): {self.tLong}, short integral (ns): {self.tShort}'])
                 labels = np.array(['L [ch]', 'S[ch]', 'T (trigger) [us]', 'baseline', 'pulse height [bits]'])
                 writer_params[i].writerow(labels[out[i] == 1])
@@ -178,7 +179,8 @@ class read_dat(object):
                 if output != False:
                     calc_params = np.array([np.array(ev[ch[i]].get_long_integral()), np.array(ev[ch[i]].get_pulse_shape()), ev[ch[i]].get_t0(), ev[ch[i]].get_baseline(), ev[ch[i]].get_pulse_height()[0]])
                     
-                    if type(cuts) != bool and i == 0:
+                    if type(cuts) != bool and i == 0:   # checks if there are cuts that need to be made and does them one at a time
+                        # Needs to be this way so we can get a specific number of events
                         L, S = self.select_events(calc_params[0], calc_params[1], 'L', 'S', cuts, inc, visual=False)
                         if len(L) == 0:
                             counter -= 1
@@ -292,28 +294,28 @@ class read_dat(object):
         - Finally press "q" to quit.
         """
         if mode == 'm':
-            if len(x_param) == 0 or len(y_param) == 0:
-                print('ERROR: No x_param and y_param values recieved. x_param and y_param values required for manual cut mode. ')
+            if len(x_param) == 0 or len(y_param) == 0:  # checking if there were arrays input, otherwise it doesn't work
+                print('ERROR: No x_param and y_param values recieved. x_param and y_param values required for manual cut mode. Returning None.')
+                return None
             else:
-                fig = plt.figure(1)
-                # plt = fig.add_subplot(111)
-                cmap_r = cm.get_cmap('Blues_r')
-                plt.hist2d(x_param, y_param, [256,256], lims, norm=colors.LogNorm(vmin=1), cmap=cmap_r)
-                plt.colorbar()
-                # plt.title(r"Press $a$ to add a cut, $x$ to end the cut, $u$ to undo the last point added to the current cut, $d$ ")
-                plt.xlabel(f'{x_param_name} [ch]')
-                plt.ylabel(f'{y_param_name} [ch]')
-                cip = fig.canvas.mpl_connect('key_press_event', self.__press)
-                plt.show(block=True)
+                if len(x_param) != len(y_param):    # checking length of input arrays
+                    print('ERROR: x_param and y_param need to be the same length. Returning None')
+                    return None
+                else:
+                    fig = plt.figure(1)
+                    cmap_r = cm.get_cmap('Blues_r')
+                    plt.hist2d(x_param, y_param, [256,256], lims, norm=colors.LogNorm(vmin=1), cmap=cmap_r)
+                    plt.colorbar()
+                    plt.xlabel(f'{x_param_name} [ch]')
+                    plt.ylabel(f'{y_param_name} [ch]')
+                    cip = fig.canvas.mpl_connect('key_press_event', self.__press)
+                    plt.show(block=True)
 
-
-        # elif mode == "a":
-        #     print("Auto mode not integrated yet")
         elif mode == 'p':
             if file == False:
                 file = input('An input file is required to run in mode "p"\nPlease enter your filename.csv:')
             else:
-                if file[-3:] != 'csv':
+                if file[-3:] != 'csv':  # checking if the filename has .csv, if not it adds it
                     file = file + '.csv'
                 with open(file, 'r') as read_file:
                     csv_reader = csv.reader(read_file)
@@ -329,7 +331,7 @@ class read_dat(object):
         
         # Turning points into polygons
         indices = np.cumsum(self.cuts)
-        split_cuts = np.split(self.selection, indices, axis=1)[:-1]
+        split_cuts = np.split(self.selection, indices, axis=1)[:-1]     # splitting the selections into their separate cuts. It's inefficient but a mess to redo so I'm leaving it
         for split in split_cuts:
             self.polygon_cuts.append(pat.Polygon(np.transpose(split)))
         
@@ -370,6 +372,7 @@ class read_dat(object):
                 print(f'Begin selection for cut {len(self.cuts)}')
                 
                 cid = fig.canvas.mpl_connect('button_press_event', self.__onclick)
+
         elif event.key == 'x' or event.key == 'X':
             if len(self.cuts) == 1:
                 if len(self.selection[0]) < 3:
@@ -383,10 +386,12 @@ class read_dat(object):
             print('End selection')
             cid = fig.canvas.mpl_connect('button_press_event', self.__onclick)
             fig.canvas.mpl_disconnect(cid)
+
         elif event.key == 'q' or event.key == 'Q':
             cip = fig.canvas.mpl_connect('key_press_event', self.__press)
             fig.canvas.mpl_disconnect(cip)
             plt.close()
+
         elif event.key == 'u' or event.key == 'U':
             if len(self.cuts) != 0 and self.cuts[-1] == 0:
                 l = ax.lines
@@ -396,6 +401,7 @@ class read_dat(object):
                 plt.draw()
             else:
                 print('You can\'t use "u" functionality on a completed selection. Please delete the selection using "d" and redo it.')
+
         elif event.key == 'd' or event.key == 'D':
             if len(self.cuts) != 0 and self.cuts[-1] == 0:
                 print('Press X to end the current selection and then D to delete the whole selection!')
@@ -410,8 +416,8 @@ class read_dat(object):
                 self.cuts.pop()
                 print(len(self.cuts))
                 plt.draw()
+
         elif event.key == 'o' or event.key == 'O': 
-            # f = open("cuts_SL.txt","w")
             ax = plt.gca()
             x = ax.get_xlabel()
             y = ax.get_ylabel()
@@ -470,22 +476,22 @@ class read_dat(object):
             y_param_cut : ndarray
                 Array of y_param values for events that passed the cuts applied.
         """        
-        if visual == True:
+        if visual == True:  # creating the subplots now with a check or else there were millions produced when doing lst_out
             fig, ax = plt.subplots()
-        # cut_id=cut_id[inc!=0]
-        x_param = np.array(x_param)
+
+        x_param = np.array(x_param) # numpy arrays are easier to work with but I wanted the input to not have to be.
         y_param = np.array(y_param)
-        check_points = np.transpose([x_param, y_param])
+        check_points = np.transpose([x_param, y_param]) # array of x-y pairs that will be checked whether they are in or out of the polygons
         mask = []
 
         for i in range(len(cut_id)):
-            if len(np.shape(check_points)) < 2:
+            if len(np.shape(check_points)) < 2: # checks if there is more than one point, otherwise it needs to use a different method
                 temp_mask = self.polygon_cuts[cut_id[i]].contains_point(check_points)
             else:
                 temp_mask = self.polygon_cuts[cut_id[i]].contains_points(check_points)
 
 
-            if inc[i] == -1:
+            if inc[i] == -1:    # a bit of chloe magic to determine if the point should be included or excluded based on the cuts and inc args
                 temp_mask = np.invert(temp_mask)
             if i != 0:
                 if inc[i] == 1:
@@ -495,58 +501,13 @@ class read_dat(object):
             else:
                 mask = temp_mask
         
-            if visual == True:
+            if visual == True:  # plotting outline of the polygon used
                 self.polygon_cuts[cut_id[i]].set_fill(False)
                 self.polygon_cuts[cut_id[i]].set_ec('r')
                 self.polygon_cuts[cut_id[i]].set_ls('--')
                 ax.add_patch(self.polygon_cuts[cut_id[i]])
 
-
-        """ Chloe's code. had a bug that was fixed by switching to polygons
-        for i in range(len(cut_id)):
-            indices = np.cumsum(self.cuts)
-            split = np.split(self.selection, indices, axis=1)[:-1]
-            #select the cut that we are looking at
-            # if len self.cuts==1:
-                # cut = self.selection
-            print(cut_id[i])
-            cut = np.array(split[cut_id[i]])
-
-            #split the cut into a top slice and a bottom slice
-            split_1 = np.split(cut, [np.argmax(cut[0])+1], axis=1)[0]
-            split_2 = np.flip(np.split(np.roll(cut, -1, axis=1), [np.argmax(cut[0])-1], axis=1)[1], axis=1)
-            
-
-            #Check which side is on top
-            if (split_1[1][1]-split_1[1][0])/(split_1[0][1]-split_1[0][0])>(split_2[1][1]-split_2[1][0])/(split_2[0][1]-split_2[0][0]):
-                upper_cut = split_1
-                lower_cut = split_2
-            else:
-                lower_cut = split_1
-                upper_cut = split_2
-
-            #splines to represent the upper and lower lines of the cuts
-            upp_spline = interp1d(upper_cut[0], upper_cut[1], bounds_error=False, fill_value=0)
-            low_spline = interp1d(lower_cut[0], lower_cut[1], bounds_error=False, fill_value=1)
-            
-            temp_mask = (S - low_spline(L) > 0) & (S - upp_spline(L) < 0)
-
-            if inc[i] == -1:
-                temp_mask =~ temp_mask
-            if i != 0:
-                if inc[i] == 1:
-                    mask = (mask) | (temp_mask)
-                elif inc[i] == -1:
-                    mask = (mask) & (temp_mask)
-            else:
-                mask = temp_mask
-            if visual == True:
-                plt.plot(upper_cut[0], upp_spline(upper_cut[0]), 'r--')
-                plt.plot(lower_cut[0], low_spline(lower_cut[0]), 'r--')
-        """
-
-        if visual == True:
-
+        if visual == True:  # plotting red if it's included, blue if it's excluded
             cmap_b, cmap_r = cm.get_cmap('Blues_r'), cm.get_cmap('Reds_r')
             plt.title('Cut Check')
             if x_param[mask] != []:
