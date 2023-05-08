0673b60a98998877acda1d6936dfa7280b4e80f8
mkidson <miles.kidson@gmail.com>
2023-05-08 14:20:36 +0200

    added setup.py

---
+++ dDAQ_final/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='read_dat',
+      version='1.0',
+      description='read_dat',
+      author='Miles Kidson',
+      author_email='kdsmil001@myuct.ac.za',
+     )
