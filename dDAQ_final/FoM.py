b7712dddcce30487784df73313ad50e01b44296d
mkidson <miles.kidson@gmail.com>
2023-06-20 12:54:42 +0200

    ok we're back to splitting things up because the other one didn't work

--- dDAQ_final/FoM.py
+++ dDAQ_final/FoM.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.optimize import curve_fit
+from scipy.signal import savgol_filter, find_peaks
 
 def gaussian(x, mu, sigma, A):
     return (A*(1/(sigma*np.sqrt(2*np.pi)))*np.exp(-(1/2)*((x-mu)/sigma)**2))
@@ -7,26 +8,38 @@ def gaussian(x, mu, sigma, A):
 def two_gaussians(x, mu_1, sigma_1, A_1, mu_2, sigma_2, A_2):
     return gaussian(x, mu_1, sigma_1, A_1) + gaussian(x, mu_2, sigma_2, A_2)
 
-def FoM(L, S, L_slice_size, start_energy, cutoff_energy):
+def FoM(L, S, neutron_L, neutron_S, gamma_L, gamma_S, L_slice_size, start_energy, cutoff_energy):
 
     L_slice_edges = np.arange(start_energy, cutoff_energy, L_slice_size)
 
     S_hist, S_bins = np.histogram(S, bins='sqrt')
     S_bins_for_plot = S_bins[:-1]
 
+    neutron_slices = []
+    gamma_slices = []
 
-    FoMs = np.zeros(len(L_slice_edges)-1)
+    for e in range(len(L_slice_edges)-1):
+        neutron_slices.append(np.histogram(neutron_S[(neutron_L >= L_slice_edges[e]) & (neutron_L < L_slice_edges[e+1])], bins=S_bins)[0])
+        gamma_slices.append(np.histogram(gamma_S[(gamma_L >= L_slice_edges[e]) & (gamma_L < L_slice_edges[e+1])], bins=S_bins)[0])
 
-    for i in range(len(L_slice_edges)-1):
-        fit_hist = np.histogram(S[(L >= L_slice_edges[i]) & (L < L_slice_edges[i+1])], bins=S_bins)
+    
+    FoMs = np.zeros(len(neutron_slices))
+    popts = []
+
+    for i in range(len(neutron_slices)):
+
         try:
-            popt, pcov = curve_fit(two_gaussians, S_bins_for_plot, fit_hist[0], [0.5, 0.1, 100, 0.6, 0.1, 100])
+            neutron_fit = curve_fit(gaussian, S_bins_for_plot, neutron_slices[i])
+            gamma_fit = curve_fit(gaussian, S_bins_for_plot, gamma_slices[i])
+            popts.append([neutron_fit[0], gamma_fit[0]])
         except RuntimeError as r:
             print(r)
-            print(f'L value reached: {L_slice_edges[i+1]:.5f} MeV')
+            print(f'L value reached: {L_slice_edges[i]:.5f} MeV')
             break
 
-        FoM = np.abs(popt[0] - popt[3]) / (2.35 * np.abs(popt[1]) + 2.35 * np.abs(popt[4]))
+
+        FoM = np.abs(gamma_fit[0][0] - neutron_fit[0][0]) / (2.35 * np.abs(gamma_fit[0][1]) + 2.35 * np.abs(neutron_fit[0][1]))
+        # print(f'Figure of Merit: {FoM}')
         FoMs[i] = FoM
 
-    return FoMs, L_slice_edges
+    return FoMs, L_slice_edges, np.array(neutron_slices), np.array(gamma_slices), S_bins_for_plot, np.array(popts)
